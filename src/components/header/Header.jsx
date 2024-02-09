be9cdbf9eb2a81bc003507d3d74fb9253c9b6661
BurakKultepe <bkultepe@yandex.com>
2024-02-09 12:22:18 +0300

    Header responsive structure added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,25 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import logo from '../../assets/etkinlik365-logo-3_result_page-0001-removebg-preview.png';
 import './HeaderStyles.css';
-import { Typography, TextField, Button, ThemeProvider, createTheme } from '@mui/material';
+import { Typography, TextField, Button, ThemeProvider, createTheme, Drawer, IconButton } from '@mui/material';
 import { AuthContext } from '../../context/AuthContext'; // AuthContext'ı içe aktarıyoruz
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'; // Giriş simgesini içe aktarıyoruz
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMusic, faFootballBall, faMask, faFilm, faListAlt, faSearch, faGraduationCap, faMicrophone } from '@fortawesome/free-solid-svg-icons'; // FontAwesome'dan simgeleri içe aktarıyoruz
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
+
 
 function Header() {
   const [activeLink, setActiveLink] = useState('');
   const [isFocused, setIsFocused] = useState(false);
   const location = useLocation();
   const navigate = useNavigate(); // navigate işlevini kullanmak için useNavigate'i içe aktarıyoruz
-  
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(false);
+
+
   const { isLoggedIn, handleLogout } = useContext(AuthContext); // AuthContext'ten isLoggedIn ve handleLogout'u alıyoruz
 
   React.useEffect(() => {
@@ -39,6 +45,24 @@ function Header() {
     },
   });
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
   return (
     <>
     <ThemeProvider theme={theme}>
@@ -48,14 +72,30 @@ function Header() {
         </Typography> 
       </div>
       <nav>
-      <img
-  src={logo}
-  alt="etkinlik365-logo"
-  width={100}
-  onClick={() => navigate('/')}
-  style={{ cursor: 'pointer' }}
-/>
-        <div></div>
+      {isMobileView && (
+              <IconButton
+                style={{
+                  color: "#643843",
+                  position: "absolute",
+                  left: "25px",
+                  fontSize: "32px",
+                  marginTop: "20px",
+                  
+                }}
+                className="drawer-icon"
+                onClick={toggleDrawer}
+                sx={{ "& svg": { fontSize: "40px" } }}
+              >
+                <MenuIcon />
+              </IconButton>
+            )}
+           <img
+              src={logo}
+              alt="etkinlik365-logo"
+              width={100}
+              onClick={() => navigate('/')}
+              style={{ cursor: 'pointer' }}
+            />
         <ul className="navbar">
           <li>
             <Link
@@ -189,6 +229,56 @@ function Header() {
             </div>
           </Button>
         </div>
+        
+        {isMobileView && (
+          <Drawer
+            anchor='left'
+            open={drawerOpen}
+            onClose={toggleDrawer}
+            className='drawer-content'
+            sx={{
+              "& .MuiDrawer-paper": {
+                backgroundColor: "#E7CBCB",
+                width: "230px",
+              },
+              "& .copyright": {
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+                right: 0,
+                backgroundColor: "white",
+                color: "#8d1212",
+                fontFamily: "'Arial', 'Helvetica', sans-serif",
+                padding: "10px",
+                textAlign: "center",
+              },
+            }}
+            >
+              <IconButton
+                style={{ color: "white", marginLeft: "auto"}}
+                onClick={toggleDrawer} // Burada toggleDrawer fonksiyonunu doğrudan çağırıyoruz
+              >
+                <CloseIcon />
+              </IconButton>
+              <div 
+                className='drawer-links'
+                onClick={toggleDrawer}
+                onKeyDown={toggleDrawer}
+              >
+                <Link
+                  to="/music"
+                  className={activeLink === '/music' ? 'active-link' : ''}
+                  onClick={() => setActiveLink('/music')}
+            >
+                  <FontAwesomeIcon icon={faMusic} className="navbar-icon" />
+                  <span className="icon-label"> Müzik</span>
+               </Link>
+               <div className='copyright'>
+                ©2024 Copyright Etkinlik365
+               </div>
+              </div>
+          </Drawer>
+        )}
       </nav>
     </ThemeProvider>
     </>
